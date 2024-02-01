6b80d48a1ad16f49b17771dde7ab8f2e5843efb0
Selma <selmadusper@outlook.com>
2024-02-01 18:21:09 +0100

    First week tasks

--- src/questions/week1/Finra.java
+++ src/questions/week1/Finra.java
@@ -1,4 +1,6 @@
 package questions.week1;
 
 public class Finra {
+
+    // write your code
 }
--- src/questions/week1/OddOrEven.java
+++ src/questions/week1/OddOrEven.java
@@ -1,4 +1,21 @@
 package questions.week1;
 
 public class OddOrEven {
+
+    public static void evenOrOdd(int num){
+        if (num % 2 == 0){
+            System.out.println(num + " is even number");
+        }else{
+            System.out.println(num + " is odd number");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        evenOrOdd(6);
+
+
+    }
+
+
 }
