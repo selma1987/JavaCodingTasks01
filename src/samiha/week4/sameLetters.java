c87211a3f7df49da9eb17b8c959c204754536da5
Samiha <samihamolinamerzouk@gmail.com>
2024-02-21 23:33:39 +0100

    Changes saved

---
+++ src/samiha/week4/sameLetters.java
@@ -0,0 +1,11 @@
+package samiha.week4;
+
+public class sameLetters {
+
+
+}
+/*
+Write a return method that checks if a string is built out of the same letters as another string.
+  Ex: same("abc", "cab"); ==> true
+      same("abc", "abb"); ==> false
+ */
