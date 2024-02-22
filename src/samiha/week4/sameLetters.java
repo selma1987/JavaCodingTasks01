262cfdd82c50d0851ab561715ba473db9d098f7d
Samiha <samihamolinamerzouk@gmail.com>
2024-02-22 16:05:58 +0100

    Task frequency of same <letters completed

--- src/samiha/week4/sameLetters.java
+++ src/samiha/week4/sameLetters.java
@@ -1,7 +1,29 @@
 package samiha.week4;
 
 public class sameLetters {
+    public static boolean sameLetters(String str1, String str2){
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
 
+        for (char ch : str1.toCharArray()) {
+            if (str2.indexOf(ch) == -1) {
+                return false;
+            }
+        }
+
+        for (char ch : str2.toCharArray()) {
+            if (str1.indexOf(ch) == -1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sameLetters("abc", "cab"));
+        System.out.println(sameLetters("abc", "abb"));
+    }
 
 }
 /*
